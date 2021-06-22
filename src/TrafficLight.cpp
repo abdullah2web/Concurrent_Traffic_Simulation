19ebb0537c1235955bfa5451af46091aa9f96dc3
Abdullah ALShayie <36479470+abdullah2web@users.noreply.github.com>
2021-06-22 15:03:26 +0300

    Update TrafficLight.cpp

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -12,7 +12,7 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
     std::unique_lock<std::mutex> uLock(_mutex);
-    _condition.wait(uLock, this {return !messages.empty()});
+    _condition.wait();
 
 }
 
@@ -21,7 +21,7 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
-    std::lock_quard<std::mutex> uLock(_mutex);
+    std::lock_guard<std::mutex> uLock(_mutex);
     _condition.notify_one();
     _queue.push_back(std::move(msg));
 }
